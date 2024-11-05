657e92aa904b8e6e7798a69723847b3ac40d240a
DNA_Q <quayedavid245@gmail.com>
2024-11-05 15:47:42 +0000

    Update ObjectTransformation.js to modify age values, add new person entries, and implement filterAdults function for filtering adults

--- Labs Solutions/Week 1.2/ObjectTransformation.js
+++ Labs Solutions/Week 1.2/ObjectTransformation.js
@@ -3,50 +3,69 @@ console.log("Object Transformation");
 
 // Declaring an array of objects of people
 let people = [
-    {
-        firstName: "Dave",
-        lastName: "Quaye",
-        age: 12,
-        occupation: "Frontend Developer"
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 16,
-        occupation: "Backend Developer"
-    },
-    {
-        firstName: "John",
-        lastName: "",
-        age: 19,
-        occupation: "Full Stack Developer"
-    }
+  {
+    firstName: "Dave",
+    lastName: "Quaye",
+    age: 28,
+    occupation: "Frontend Developer",
+  },
+  {
+    firstName: "Kwame",
+    lastName: "Nkrumah",
+    age: 16,
+    occupation: "Backend Developer",
+  },
+  {
+    firstName: "Ama",
+    lastName: "Mensah",
+    age: 19,
+    occupation: "Full Stack Developer",
+  },
+  {
+    firstName: "Kofi",
+    lastName: "Annan",
+    age: 22,
+    occupation: "Frontend Developer",
+  },
+  {
+    firstName: "Akosua",
+    lastName: "Adjei",
+    age: 25,
+    occupation: "Data Scientist",
+  },
 ];
 
 let person = people[0];
 // person variable is derived from the people array in array[0]
 
-
 console.log("--------------------------------------------------------\n");
 
 // a. A function that returns the full name of a person
 const fullName = (person) => {
-    console.log(`My name is ${person.firstName} ${person.lastName}`);
- }
- fullName(person);
+  console.log(`My name is ${person.firstName} ${person.lastName}`);
+};
+fullName(person);
 
 console.log("\n--------------------------------------------------------\n");
-
-
 
 // b. A function that checks if the person is an adult 18 years and above
 const isAdult = (person) => {
-    if (person.age >= 18) {
-        console.log("I am an adult");
-    } else {
-        console.log("I am not an adult");
-    }
-}
+  if (person.age >= 18) {
+    console.log("I am an adult");
+  } else {
+    console.log("I am not an adult");
+  }
+};
 isAdult(person);
 
 console.log("\n--------------------------------------------------------\n");
+
+// c. A function that filters an array of person objects to keep only those at least minAge years old
+const filterAdults = (array, minAge) => {
+  return array.filter((person) => person.age >= minAge);
+};
+console.log("Adults: ", filterAdults(people, 18));
+
+console.log("\n--------------------------------------------------------\n");
+
+
