a5eecefa1a5149cb821677de96093840182abbc2
DNA_Q <quayedavid245@gmail.com>
2024-11-05 14:42:07 +0000

    Enhance StringTransformation.js with console logs for original and capitalized strings

--- Labs Solutions/Week 1.2/StringTransformation.js
+++ Labs Solutions/Week 1.2/StringTransformation.js
@@ -1,9 +1,16 @@
 //String Transformation
-
+console.log("\n--------------------------------------------------------");
+console.log("Section 1: String Transformation");
+console.log("--------------------------------------------------------\n");
 // a. A function that takes a string as a parameter and converts the first letter of each word of the string in upper case.
 let string = "dave is a frontend developer";
 
 const capitalize = (string) => {
     return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
+console.log("Original String: ", string);
 console.log("Capitalized String: ", capitalize(string));
+
+
+console.log("\n--------------------------------------------------------\n");
+
