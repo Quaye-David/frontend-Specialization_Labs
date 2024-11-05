20da578cd23225ca29484a146c1e71a6ae07b21e
DNA_Q <quayedavid245@gmail.com>
2024-11-05 16:05:59 +0000

    Add FunctionComposition.js with compose function and utility functions for string and object transformations

---
+++ Labs Solutions/Week 1.2/FunctionComposition.js
@@ -0,0 +1,38 @@
+// FunctionComposition.js
+console.log("--------------------------------------------------------");
+console.log("Function Composition\n");
+console.log("--------------------------------------------------------");
+
+// Compose function
+const compose = (...fns) => (arg) => fns.reduceRight((acc, fn) => fn(acc), arg);
+
+// Functions from StringTransformation.js
+const capitalize = (string) => {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
+
+const reverseString = (string) => {
+    return string.split('').reverse().join('');
+};
+
+// Functions from ObjectTransformation.js
+const fullName = (person) => `${person.firstName} ${person.lastName}`;
+const isAdult = (person) => person.age >= 18;
+
+// Combining functions to create new functionality
+const reverseAndCapitalize = compose(capitalize, reverseString);
+const getFullNameAndCheckAdult = compose(isAdult, fullName);
+
+// Test the composed functions
+console.log(reverseAndCapitalize("JavaScript Frontend"));
+
+let person = {
+    firstName: "Kwame",
+    lastName: "Nkrumah",
+    age: 16,
+    occupation: "Backend Developer"
+};
+
+console.log(getFullNameAndCheckAdult(person));
+
+console.log("--------------------------------------------------------\n");
