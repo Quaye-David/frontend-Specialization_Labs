16ccc0c2d6f7f71a80a2ddca0fa93849138606aa
DNA_Q <quayedavid245@gmail.com>
2024-11-05 14:33:05 +0000

    Refactor capitalizeFirstLetter function to capitalize the first letter of each word in a string

--- Labs Solutions/Week 1.2/StringTransformation.js
+++ Labs Solutions/Week 1.2/StringTransformation.js
@@ -1,7 +1,9 @@
 //String Transformation
 
 // a. A function that takes a string as a parameter and converts the first letter of each word of the string in upper case.
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+let string = "dave is a frontend developer";
+
+const capitalize = (string) => {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
-capitalizeFirstLetter('dave is a developer');
+console.log("Capitalized String: ", capitalize(string));
