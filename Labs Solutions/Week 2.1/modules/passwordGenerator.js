db7a202fc263b9427e689eb93d58f66fe51c7a78
DNA_Q <david.quaye@amalitech.com>
2024-12-02 16:26:47 +0000

    Refactor PasswordGenerator constructor and generate method for improved readability and maintainability

--- Labs Solutions/Week 2.1/modules/passwordGenerator.js
+++ Labs Solutions/Week 2.1/modules/passwordGenerator.js
@@ -6,29 +6,36 @@ const CHAR_SETS = {
 };
 
 export class PasswordGenerator {
-  constructor(options = {}) {
-    this.length = options.length || 10;
-    this.options = {
-      uppercase: options.uppercase !== undefined ? options.uppercase : true,
-      lowercase: options.lowercase !== undefined ? options.lowercase : true,
-      numbers: options.numbers !== undefined ? options.numbers : true,
-      symbols: options.symbols !== undefined ? options.symbols : false
-    };
+  constructor({
+    length = 10,
+    uppercase = true,
+    lowercase = true,
+    numbers = true,
+    symbols = false
+  } = {}) {
+    this.length = length;
+    this.options = { uppercase, lowercase, numbers, symbols };
   }
 
-    generate() {
-      const enabledSets = Object.keys(this.options).filter(key => this.options[key]);
-      if (enabledSets.length === 0) {
-        console.error('No character sets selected');
-        return '';
-      }
-  
-      const chars = enabledSets.map(set => CHAR_SETS[set]).join('');
-  
-      const password = Array.from({ length: this.length }, () => 
-        chars[Math.floor(Math.random() * chars.length)]
-      ).join('');
-  
-      return password;
+  generate() {
+    const { options } = this;
+    const enabledSets = Object.keys(options).filter(key => options[key]);
+    
+    if (enabledSets.length === 0) {
+      console.error('No character sets selected');
+      return '';
     }
+
+    const chars = enabledSets.map(set => {
+      const { [set]: charSet } = CHAR_SETS;
+      return charSet;
+    }).join('');
+
+    const [...password] = Array.from(
+      { length: this.length }, 
+      () => chars[Math.floor(Math.random() * chars.length)]
+    );
+
+    return password.join('');
   }
+}
